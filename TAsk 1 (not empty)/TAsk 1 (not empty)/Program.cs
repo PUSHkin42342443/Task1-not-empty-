566f0fb5e62cae4ec9bb2e64b3180e2a2b5bb737
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-11-22 15:52:53 +0500

    выполнено задние 1 ( на этот раз точно, надеюсь)

---
+++ TAsk 1 (not empty)/TAsk 1 (not empty)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAsk_1__not_empty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine(@"'Что это такое?'");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("В этот лес завороженный,");
+            Console.WriteLine("По пушинкам серебра,");
+            Console.WriteLine("Я с винтовкой заряженной");
+            Console.WriteLine("На охоту шел вчера.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
